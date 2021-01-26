bb7635c2bbe7cea494561b6b72573ce57c58c16f
Nicholas <nicholasgfx@hotmail.com>
2021-01-25 20:48:36 -0600

    initial commmit

---
+++ script.js
@@ -0,0 +1,19 @@
+function computerPlay(playerSelection) {
+    const computerSelection = Math.floor(Math.random(3) * Math.floor(max))
+    console.log(computerSelection);
+    switch(computerSelection) {
+        case 0:
+            computerSelection = 'rock';
+            break;
+        case 1:
+            computerSelection = 'paper';
+            break;
+        case 2:
+            computerSelect = 'scissors';
+    }
+    if (playerselection)
+}
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
